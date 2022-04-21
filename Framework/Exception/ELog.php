933f7b8151c302bfd1eca6d1ea268ff116eb4c56
jh <a@j-9.cn>
2022-04-21 20:44:06 +0800

    *

--- Framework/Exception/ELog.php
+++ Framework/Exception/ELog.php
@@ -235,7 +235,7 @@ tpl;
             return $content = sprintf($content_tpl_404,
                 $cli ? 'cli' : $req->getClientIPAddress(),
                 $method,
-                $req->getHostInfo() . ($_SERVER['PATH_INFO'] ?? '$PATH_INFO$'),
+                $req->getHostInfo() . ($_SERVER['PATH_INFO'] ?? $req->getIndexName()),
                 $cli ? 'cli' : $req->getUrlReferrer()
             );
         }
